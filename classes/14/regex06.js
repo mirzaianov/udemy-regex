de81a548d7ba880b881adfbd0235a9f0818f653e
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-09-19 11:56:22 +0300

    refactor: Update lesson 14

--- classes/14/regex06.js
+++ classes/14/regex06.js
@@ -12,12 +12,15 @@ hoo
 // Character Classes
 // [abc] - Character class. One of the characters inside the square brackets - a, b or c
 
-const test = data.match(/[fcl]oo/gm);
+const test = data.match(/[fcdplb]oo/gm);
 
 console.log(test);
 
 /**
 foo
 coo
+doo
+poo
 loo
+boo
 */
--- classes/14/regex07.js
+++ classes/14/regex07.js
@@ -12,12 +12,15 @@ hoo
 // Character Classes
 // [abc] - Character class. One of the characters inside the square brackets - a, b or c
 
-const test = data.match(/[fcl]oo/gm);
+const test = data.match(/[^mh]oo/gm);
 
 console.log(test);
 
 /**
 foo
 coo
+doo
+poo
 loo
+boo
 */
